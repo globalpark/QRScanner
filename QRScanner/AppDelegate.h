fc9a2bf576225cf9233ef2b3d9824e613eb7dcf7
Luis Kentzler <luis@kentzler.me>
2014-09-29 12:39:02 -0500

    initial commit

---
+++ QRScanner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QRScanner
+//
+//  Created by Luis Kentzler on 9/29/14.
+//  Copyright (c) 2014 GlobalPark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QRScanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRScanner
+//
+//  Created by Luis Kentzler on 9/29/14.
+//  Copyright (c) 2014 GlobalPark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
